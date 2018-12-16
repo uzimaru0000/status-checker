354d6c2533004497ac0bfcd3e70afb8b3873c566
mi0-1123 <si2.o.mjt@gmail.com>
2018-12-16 17:17:26 +0900

    [add] routingを追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import MyItem from '@/components/status/MyItem'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/myitem',
+      name: 'MyItem',
+      conponent: MyItem
     }
   ]
 })
