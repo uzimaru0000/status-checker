3957054adcb34f19148adcc4b8c541033dd19e5e
ShujiOba <shuji365630@gmail.com>
2018-12-16 13:24:59 +0900

    [add] navbarを追加

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
+    <navbar :user="user"/>
     <router-view v-if="initFlag" :user="user"/>
     <b-loading v-else :is-full-page="true" :active="true"></b-loading>
   </div>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar";
 import firebase from "./firebase";
 
 export default {
   name: "App",
+  components: { Navbar },
   data() {
     return {
       user: null,
