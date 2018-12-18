369d7ff29a0e0cfd9acd3ef08d5b5262d8dc7b06
ShujiOba <shuji365630@gmail.com>
2018-12-18 20:55:42 +0900

    [add] model bindingできるようにした

--- src/components/status/Detailes.vue
+++ src/components/status/Detailes.vue
@@ -2,32 +2,58 @@
   <div class="box">
     <div class="columns">
       <div class="column is-one-third">
-        <p class="image is-fullwidth">
-          <img src="https://bulma.io/images/placeholders/128x128.png">
-        </p>
+        <figure class="image is-fullwidth">
+          <img class="is-rounded" :src="user.imageURL">
+        </figure>
       </div>
 
-      <div class="column columns is-multiline is-vcentered">
-        <div class="column is-full">
-          <b-field label="Name" type="is-danger">
-            <b-input v-model="user.name"></b-input>
-          </b-field>
-        </div>
-        <div class="column is-full">
-          <b-field label="Motivation">
-            <slide-bar size="is-large" type="is-primary" v-model.number="user.motivation"/>
-          </b-field>
-        </div>
-        <div class="column is-full">
-          <b-field label="Note">
-            <b-input type="textarea"></b-input>
-          </b-field>
+      <div class="column">
+        <div class="columns is-multiline">
+          <div class="column is-full">
+            <b-field
+              label="Name"
+              :type="user.name.length === 0 ? 'is-danger' : 'is-primary'"
+              :message="user.name.length === 0 ? '入力してください' : ''"
+            >
+              <b-input v-model="user.name"></b-input>
+            </b-field>
+          </div>
+          <div class="column is-full">
+            <b-field label="Motivation">
+              <slide-bar
+                size="is-large"
+                :type="motivationColor(user.motivation)"
+                v-model.number="user.motivation"
+              />
+            </b-field>
+          </div>
+          <div class="column is-full">
+            <b-field label="Skill">
+              <div class="control">
+                <input type="text" class="input" v-model="inputSkill" @keydown.enter="enter">
+              </div>
+            </b-field>
+          </div>
+          <div class="column is-full">
+            <div class="box" style="height:120px">
+              <b-taglist>
+                <b-tag
+                  v-for="(skill, i) in user.skill"
+                  :key="skill"
+                  type="is-primary"
+                  size="is-medium"
+                  closable
+                  @close="delTag(i)"
+                >{{ skill }}</b-tag>
+              </b-taglist>
+            </div>
+          </div>
         </div>
       </div>
     </div>
     <div class="content">
       <b-field label="Comment">
-        <b-input maxlength="200" type="textarea" v-model="user.comment"></b-input>
+        <b-input maxlength="140" type="text" v-model="user.comment"></b-input>
       </b-field>
     </div>
   </div>
@@ -38,7 +64,29 @@ import SlideBar from "./SlideBar";
 
 export default {
   props: { user: Object },
-  components: { SlideBar }
+  components: { SlideBar },
+  data() {
+    return {
+      inputSkill: ""
+    };
+  },
+  methods: {
+    motivationColor(motivation) {
+      if (motivation >= 30) return "is-primary";
+      else if (motivation >= 10) return "is-warning";
+      else return "is-danger";
+    },
+    enter(e) {
+      if (/\S+/.test(this.inputSkill)) {
+        this.user.skill = (this.user.skill || []).concat(this.inputSkill);
+
+        this.inputSkill = "";
+      }
+    },
+    delTag(i) {
+      this.user.skill.splice(i, 1);
+    }
+  }
 };
 </script>
 
